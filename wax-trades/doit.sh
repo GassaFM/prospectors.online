9b12da6aad12e0c8ff7ebc0fec2b8d9bab2be5e1
Ivan Kazmenko <gassa@mail.ru>
2020-07-11 02:34:36 +0300

    added station deals to trades

--- wax-trades/doit.sh
+++ wax-trades/doit.sh
@@ -2,6 +2,7 @@
 cd "${0%/*}"
 ./refresh_log_buys "https://mainnet.wax.dfuse.io/graphql" | tee -a buys-log.txt || exit 1
 ./refresh_log_sales "https://mainnet.wax.dfuse.io/graphql" | tee -a sales-log.txt || exit 1
+./refresh_log_station "https://mainnet.wax.dfuse.io/graphql" | tee -a station-log.txt || exit 1
 ./display_deals || exit 1
 mv *.html ../public_html/wax/trades/ || exit 1
 shopt -s dotglob nullglob
