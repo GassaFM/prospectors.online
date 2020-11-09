986fc62883412707eb41e6c6dd747c6c065a077e
Ivan Kazmenko <gassafm@gmail.com>
2020-11-10 02:31:17 +0300

    boomtown stores: fix for long URLs

--- boom-stores/getter-allscopes.sh
+++ boom-stores/getter-allscopes.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 export DFUSETOKEN=`cat ../dfuse.token`
-curl --get \
-     -k \
+curl -X POST \
      -H "Authorization: Bearer $DFUSETOKEN" \
-     --data-urlencode "account=prospectorsq" \
-     --data-urlencode "table=$1" \
-     --data-urlencode "scopes=`../stores/scopelist_piper < $1.scopelist.json`" \
-     --data-urlencode "json=false" \
-     --data-urlencode "key_type=$2" \
+     -d "account=prospectorsq&table=$1&scopes=`../stores/scopelist_piper < $1.scopelist.json`&json=false&key_type=$2" \
      --compressed \
      "https://eos.dfuse.eosnation.io/v0/state/tables/scopes" \
      > $1.allscopes.binary
