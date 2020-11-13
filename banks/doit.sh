94255375d2c373479a1278d5e048b776016579ef
Ivan Kazmenko <gassafm@gmail.com>
2020-11-13 20:51:34 +0300

    eos: fix server to retrieve bank logs

--- banks/doit.sh
+++ banks/doit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "${0%/*}"
-./refresh_log_banks "https://mainnet.eos.dfuse.io/graphql" 109336179 | tee -a banks-log.txt || exit 1
+./refresh_log_banks "https://eos.dfuse.eosnation.io/graphql" 109336179 | tee -a banks-log.txt || exit 1
 ./show_log_banks || exit 1
 mv *.html ../public_html/banks/ || exit 1
