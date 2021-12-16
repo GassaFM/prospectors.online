208bf5ef7e3485b78b4f9ebf8615bef5dba986ed
Ivan Kazmenko <gassafm@gmail.com>
2021-12-16 22:10:17 +0300

    fixed stores for >1500 accounts at station

--- grand-stores/getter-allscopes.sh
+++ grand-stores/getter-allscopes.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 export DFUSETOKEN=`cat ../dfuse.token`
-curl -X POST \
-     -H "Authorization: Bearer $DFUSETOKEN" \
-     -d "account=prospectorsn&table=$1&scopes=`../stores/scopelist_piper < $1.scopelist.json`&json=false&key_type=$2" \
-     --compressed \
-     "https://wax.dfuse.eosnation.io/v0/state/tables/scopes" \
-     > $1.allscopes.binary
+step=$((500))
+i=$((0))
+rm -f $1.partnames
+while true ; do
+	j=$(( $i + $step ))
+	name=$1.allscopes.$i.$j.binary
+	scopes=`../stores/scopelist_piper $i $j < $1.scopelist.json`
+	if [ "$scopes" == "" ] ; then
+		break
+	fi
+	curl --get \
+	     -k \
+	     -H "Authorization: Bearer $DFUSETOKEN" \
+	     --data-urlencode "account=prospectorsn" \
+	     --data-urlencode "table=$1" \
+	     --data-urlencode "scopes=$scopes" \
+	     --data-urlencode "json=false" \
+	     --data-urlencode "key_type=$2" \
+	     --compressed \
+	     "https://wax.dfuse.eosnation.io/v0/state/tables/scopes" \
+	     > $name
+	i=$(( $j ))
+	echo $name >> $1.partnames
+done
+cat $1.partnames | xargs ../stores/table_gluer $1.allscopes.binary
